7feb901d156d7b3a865886bc1c3dbf4b878eb553
Leander Dylan Broñola <leanderdylan.01@gmail.com>
2026-09-13 18:17:29 +0800

    feat: Initialize attendance system and database

---
+++ backend/app/Models/Attendance.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'type',
+        'scanned_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
